66bc5400f2068428cd780738eb7c38db0d69bd59
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-04-05 21:54:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,10 +19,9 @@ class App < Sinatra::Base
   end
 
   get '/set' do
-    # set the :foo key of the session hash equal to 'hello' here!
+    session[:foo] == 'hello'# set the :foo key of the session hash equal to 'hello' here!
     if session[:foo] == 'hello'
       redirect '/fetch'
-  #     redirect '/set'
 
     else
       "Session value has not been set!"
